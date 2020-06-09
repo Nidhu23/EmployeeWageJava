bc6d13801f0d9231ad2184add421b55563ae0d5e
“Nidhu23” <nidhunanik@gmail.com>
2020-06-09 21:32:12 +0530

    Added: Monthly wage computation code

--- EmployeeWageUC1.java
+++ EmployeeWageUC1.java
@@ -1,10 +1,10 @@
 public class EmployeeWageUC1 {
 
 	public static void main(String[] args) {
-		int ratePerHr=20,empHr=0;
+		int ratePerHr=20,empHr=0,Total_hrs=0;
 		System.out.println("Welcome to Employee Wage Problem");
-		int full_time=1;
-		int part_time=2;
+		int full_time=1,part_time=2,Working_days=20;
+		for (int i=0;i<Working_days;i++) {
 		double empCheck=Math.floor(Math.random()*10)%3;
 		switch((int)empCheck) {
 		case 1: empHr=8;
@@ -13,7 +13,9 @@ public class EmployeeWageUC1 {
 				break;
 		default:empHr=0;
 				}
-		System.out.println("DailyWage="+ratePerHr*empHr);
+		Total_hrs+=empHr;
+		}
+		System.out.println("Monthly Wage="+(ratePerHr*Total_hrs));
 	 }
 
 }

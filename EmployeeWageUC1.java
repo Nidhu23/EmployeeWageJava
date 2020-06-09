f023fa0b8ebe441296faed57980c9fa54985cb63
“Nidhu23” <nidhunanik@gmail.com>
2020-06-09 20:53:33 +0530

    Added: Code to compute daily wage

--- EmployeeWageUC1.java
+++ EmployeeWageUC1.java
@@ -1,14 +1,17 @@
-import java.util.*;
-public class EmployeeWageUC1{
-	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wage Problem");
-	int full_time=1;
-	double empCheck=Math.floor(Math.random()*10)%2;
-	if (empCheck==full_time){
-		System.out.println("Present");
-		}
-	else
-		System.out.println("Absent");
+public class EmployeeWageUC1 {
 
-	}
+	public static void main(String[] args) {
+		int ratePerHr=20,empHr=0;
+		System.out.println("Welcome to Employee Wage Problem");
+		int full_time=1;
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if (empCheck==full_time){
+			empHr=8;
+		    }
+		else {
+			empHr=0;
+		    }
+		System.out.println("DailyWage="+ratePerHr*empHr);
+	 }
+
 }

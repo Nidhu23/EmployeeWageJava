8e238897f8d092da5356718c3071cb60475d52b5
“Nidhu23” <nidhunanik@gmail.com>
2020-06-09 21:06:08 +0530

    Added: part time employee

--- EmployeeWageUC1.java
+++ EmployeeWageUC1.java
@@ -4,13 +4,18 @@ public class EmployeeWageUC1 {
 		int ratePerHr=20,empHr=0;
 		System.out.println("Welcome to Employee Wage Problem");
 		int full_time=1;
-		double empCheck=Math.floor(Math.random()*10)%2;
+		int part_time=2;
+		double empCheck=Math.floor(Math.random()*10)%3;
 		if (empCheck==full_time){
 			empHr=8;
 		    }
+		else if(empCheck==2){
+			empHr=4;
+		        
+		    }
 		else {
 			empHr=0;
-		    }
+		}
 		System.out.println("DailyWage="+ratePerHr*empHr);
 	 }
 
